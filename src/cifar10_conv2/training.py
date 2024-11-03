3790c47c7cfdb7be41b432de8162a4a90e779e74
eugen <bb.octav@gmail.com>
2024-11-03 19:32:54 +0200

    deleted wanb metadata

--- src/cifar10_conv2/training.py
+++ src/cifar10_conv2/training.py
@@ -12,8 +12,10 @@ from src.cifar10_conv2.model_conv2 import ModelCifar10Conv2
 import numpy as np
 from torch.optim.lr_scheduler import StepLR
 from src.training_common import get_model_parameters_and_masks
+import wandb
 
 STOP_EPOCH = 10
+EXPONENT_CONSTANT = 3
 
 def get_model_remaining_parameters_percentage(model:ModelCifar10Conv2):
     total, remaining = model.get_parameters_pruning_statistics()
@@ -38,8 +40,7 @@ def train(model:ModelCifar10Conv2, train_loader, optimizer, epoch):
         loss = criterion(output, target)
         loss_remaining_weights = model.get_remaining_parameters_loss()
 
-        loss_remaining_weights *= (epoch ** 2.5)
-        # loss_remaining_weights *= (epoch * 15)
+        loss_remaining_weights *= (epoch ** EXPONENT_CONSTANT)
 
         if(epoch > STOP_EPOCH):
             loss_remaining_weights *= 0
@@ -61,19 +62,26 @@ def train(model:ModelCifar10Conv2, train_loader, optimizer, epoch):
                 continue
 
             total, remaining = model.get_parameters_pruning_statistics()
-            percent = remaining / total
-            print(f'Masked weights percentage: {percent*100:.2f}%,Loss remaining: {average_loss_masks}, Loss data: {average_loss_dataset}')
+            pruned_percent = remaining / total
+            print(f'Masked weights percentage: {pruned_percent*100:.2f}%,Loss remaining: {average_loss_masks}, Loss data: {average_loss_dataset}')
 
             total, remaining = model.get_parameters_flipped_statistics()
-            percent = remaining / total
-            print(f'Flipped weights percentage: {percent*100:.2f}%,Loss remaining: {average_loss_masks}, Loss data: {average_loss_dataset}')
+            non_flip_percentage = remaining / total
+            print(f'Flipped weights percentage: {non_flip_percentage*100:.2f}%')
 
-
+            wandb.log({
+                'epoch': epoch,
+                'train_dataset_loss': average_loss_dataset,
+                'remaining_weights_loss': average_loss_masks,
+                'pruned_percent': pruned_percent,
+                'non_flipped_percent': non_flip_percentage,
+                'batch_idx': batch_idx
+            })
 
             average_loss_masks = 0
             average_loss_dataset = 0
 
-def test(model, test_loader):
+def test(model, test_loader, epoch):
     model.eval()
     criterion = nn.CrossEntropyLoss(reduction='sum')
     device = get_device()
@@ -92,6 +100,12 @@ def test(model, test_loader):
     correct = correct.item()
     accuracy = 100. * correct / len(test_loader.dataset)
 
+    wandb.log({
+        'epoch': epoch,
+        'test_loss': test_loss,
+        'accuracy': accuracy,
+    })
+
     print(f'\nTest set: Average loss: {test_loss:.4f}, '
           f'Accuracy: {correct}/{len(test_loader.dataset)}'
           f' ({accuracy:.0f}%)\n')
@@ -105,7 +119,22 @@ def run_cifar10_conv2():
     lr_pruning_params = 0.01
     lr_flipping_params = 0.02
 
-    num_epochs = 100
+    num_epochs = 30
+    scheduler_decay_while_pruning = 0.9
+    scheduler_decay_after_pruning = 0.9
+
+    wandb.init(project='mnist-fcn', config={
+        'total_epochs': num_epochs,
+        'stop_pruning_epoch': STOP_EPOCH,
+        'lr_weight_bias': lr_weight_bias,
+        'lr_pruning_params': lr_pruning_params,
+        'lr_flipping_params': lr_flipping_params,
+        'exponent_constant': EXPONENT_CONSTANT,
+        'scheduler_decay_while_pruning': scheduler_decay_while_pruning,
+        'scheduler_decay_after_pruning': scheduler_decay_after_pruning,
+        'scaled_training_loss': True
+    })
+
     optimizer = torch.optim.AdamW([
                         {'params': weight_bias_params, 'lr': lr_weight_bias},
                         {'params': pruning_params, 'lr': lr_pruning_params},
@@ -114,9 +143,9 @@ def run_cifar10_conv2():
 
     def lambda_lr_weight_bias(epoch):
         if epoch < STOP_EPOCH:
-            return (0.9 ** epoch)
+            return (scheduler_decay_while_pruning ** epoch)
         else:
-            return (0.25 ** ((epoch-STOP_EPOCH) // 2))
+            return (scheduler_decay_after_pruning ** (epoch-STOP_EPOCH))
 
     def lambda_lr_pruning_params(epoch):
         if epoch < STOP_EPOCH:
@@ -131,7 +160,7 @@ def run_cifar10_conv2():
 
     for epoch in range(1, num_epochs + 1):
         train(model, train_loader, optimizer, epoch)
-        test(model, test_loader)
+        test(model, test_loader, epoch)
         scheduler.step()
 
     print("Training complete")
--- src/mnist_fcn/training.py
+++ src/mnist_fcn/training.py
@@ -5,17 +5,20 @@ from torchvision import datasets, transforms
 from src.others import get_device
 from .model_fcn import ModelMnistFNN
 import numpy as np
-
+import wandb
 from ..constants import WEIGHTS_ATTR, BIAS_ATTR, WEIGHTS_PRUNING_ATTR, WEIGHTS_FLIPPING_ATTR
 from ..data_preprocessing import preprocess_mnist
 from ..layers import ConfigsNetworkMasks
+from ..training_common import get_model_parameters_and_masks
 
 STOP_EPOCH = 10
+EXPONENT_CONSTANT = -1
 
 def train(model: ModelMnistFNN, train_loader, optimizer, epoch):
     model.train()
     criterion = nn.CrossEntropyLoss()
     device = get_device()
+
 
     average_loss_masks = 0
     average_loss_dataset = 0
@@ -28,9 +31,9 @@ def train(model: ModelMnistFNN, train_loader, optimizer, epoch):
         optimizer.zero_grad()
         output = model(data)
 
-        loss = criterion(output, target)
+        loss = criterion(output, target) * (epoch ** 1.8)
         loss_remaining_weights = model.get_remaining_parameters_loss()
-        loss_remaining_weights = loss_remaining_weights * (epoch ** 1.5)
+        loss_remaining_weights = loss_remaining_weights * (epoch ** EXPONENT_CONSTANT)
 
         if(epoch > STOP_EPOCH):
             loss_remaining_weights *= 0
@@ -51,15 +54,24 @@ def train(model: ModelMnistFNN, train_loader, optimizer, epoch):
             print(f'Train Epoch: {epoch} [{batch_idx*len(data)}/{len(train_loader.dataset)}]')
             total, remaining = model.get_parameters_pruning_statistics()
             pruned_percent = remaining / total
-            print(f'Masked weights percentage: {pruned_percent*100:.2f}%,Loss pruned: {loss_remaining_weights.item()}, Loss data: {loss.item()}')
+            print(f'Masked weights percentage: {pruned_percent*100:.2f}%,Loss pruned: {average_loss_masks}, Loss data: {average_loss_dataset}')
             total, remaining = model.get_parameters_flipped_statistics()
-            flip_percentage = remaining / total
-            print(f'Flipped weights percentage: {flip_percentage*100:.2f}%')
+            non_flip_percentage = remaining / total
+            print(f'Flipped weights percentage: {non_flip_percentage*100:.2f}%')
+
+            wandb.log({
+                'epoch': epoch,
+                'train_dataset_loss': average_loss_dataset,
+                'remaining_weights_loss': average_loss_masks,
+                'pruned_percent': pruned_percent,
+                'non_flipped_percent': non_flip_percentage,
+                'batch_idx': batch_idx
+            })
 
             average_loss_masks = 0
             average_loss_dataset = 0
 
-def test(model, test_loader):
+def test(model, test_loader, epoch):
     model.eval()
     criterion = nn.CrossEntropyLoss(reduction='sum')
     test_loss = 0
@@ -74,31 +86,44 @@ def test(model, test_loader):
     test_loss /= len(test_loader.dataset)
 
     accuracy = 100. * correct / len(test_loader.dataset)
-    global exp
-    if accuracy <= 97.5:
-        # exp += 1
-        # print("EXPONENT INCREASED")
-        pass
 
     print(f'\nTest set: Average loss: {test_loss:.4f}, '
           f'Accuracy: {correct}/{len(test_loader.dataset)}'
           f' ({100. * correct / len(test_loader.dataset):.0f}%)\n')
 
+    wandb.log({
+        'epoch': epoch,
+        'test_loss': test_loss,
+        'accuracy': accuracy,
+    })
+
 
 def run_mnist():
     # Define transformations for the training and testing data
     train_loader, test_loader = preprocess_mnist()
+    model = ModelMnistFNN(ConfigsNetworkMasks(mask_pruning_enabled=True, mask_flipping_enabled=True, weights_training_enabled=True)).to(get_device())
+    weight_bias_params, prune_params, flip_params = get_model_parameters_and_masks(model)
 
     lr_weight_bias = 0.005
     lr_pruning_params = 0.01
     lr_flipping_params = 0.02
-    num_epochs = 100
+    num_epochs = 30
 
-    weight_bias_params = []
-    prune_params = []
-    flip_params = []
+    scheduler_decay_while_pruning = 0.9
+    scheduler_decay_after_pruning = 0.9
 
-    model = ModelMnistFNN(ConfigsNetworkMasks(mask_pruning_enabled=True, mask_flipping_enabled=True, weights_training_enabled=True)).to(get_device())
+    wandb.init(project='mnist-fcn', config={
+        'total_epochs': num_epochs,
+        'stop_pruning_epoch': STOP_EPOCH,
+        'lr_weight_bias': lr_weight_bias,
+        'lr_pruning_params': lr_pruning_params,
+        'lr_flipping_params': lr_flipping_params,
+        'exponent_constant': EXPONENT_CONSTANT,
+        'scheduler_decay_while_pruning': scheduler_decay_while_pruning,
+        'scheduler_decay_after_pruning': scheduler_decay_after_pruning,
+        'scaled_training_loss': True
+    })
+
     for name, param in model.named_parameters():
         if WEIGHTS_ATTR in name or BIAS_ATTR in name:
             weight_bias_params.append(param)
@@ -115,9 +140,9 @@ def run_mnist():
 
     def lambda_lr_weight_bias(epoch):
         if epoch < STOP_EPOCH:
-            return (0.9 ** epoch)
+            return (scheduler_decay_while_pruning ** epoch)
         else:
-            return (0.9 ** (epoch-STOP_EPOCH))
+            return (scheduler_decay_after_pruning ** (epoch-STOP_EPOCH))
 
     def lambda_lr_pruning_params(epoch):
         if epoch < STOP_EPOCH:
@@ -133,8 +158,9 @@ def run_mnist():
     for epoch in range(1, num_epochs + 1):
         # Toggle mask as needed
         train(model, train_loader, optimizer, epoch)
-        test(model, test_loader)
+        test(model, test_loader, epoch)
         scheduler.step()
 
     print("Training complete")
+    wandb.finish()
   
