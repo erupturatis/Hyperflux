72f09df423f107b9eca464daf40378057acf31fe
xerxxy <xerx.2003@yahoo.com>
2025-01-20 13:36:51 +0200

    bug fix / total

--- src/resnet50_cifar100/train_pruned_resnet50_cifar100.py
+++ src/resnet50_cifar100/train_pruned_resnet50_cifar100.py
@@ -24,7 +24,7 @@ def initialize_model():
         weights_training_enabled=True,
     )
     MODEL = Resnet50Cifar100(configs_network_masks).to(get_device())
-    MODEL.load(BASELINE_RESNET18_CIFAR10, BASELINE_MODELS_PATH)
+    MODEL.load("resnet50_cifar100_accuracy78.3%", BASELINE_MODELS_PATH)
 
 def get_epoch() -> int:
     global epoch_global
@@ -60,7 +60,7 @@ def initialize_training_context():
     training_context = TrainingContextPrunedTrain(
         TrainingContextPrunedTrainArgs(
             lr_weights_reset=lr_weights_finetuning,
-            lr_flow_params_reset=get_lr_flow_params_reset(),
+            lr_flow_params_reset=get_lr_flow_params(),
             l0_gamma_scaler=0,
             optimizer_weights=optimizer_weights,
             optimizer_flow_mask=optimizer_flow_mask
@@ -74,7 +74,7 @@ def initialize_stages_context():
     regrowing_end = sparsity_configs["regrowing_end"]
     regrowth_stage_length = regrowing_end - pruning_end
 
-    pruning_scheduler = PressureScheduler(pressure_exponent_constant=1.5, sparsity_target=sparsity_configs["target_sparsity"], epochs_target=pruning_end)
+    pruning_scheduler = PressureScheduler(pressure_exponent_constant=1.5, sparsity_target=sparsity_configs["target_sparsity"], epochs_target=pruning_end, step_size=0.1)
     scheduler_decay_after_pruning = sparsity_configs["lr_flow_params_decay_regrowing"]
 
     scheduler_weights_lr_during_pruning = CosineAnnealingLR(training_context.get_optimizer_weights(), T_max=pruning_end, eta_min=1e-7)
@@ -93,7 +93,7 @@ def initialize_stages_context():
         )
     )
 
-MODEL: Resnet18Cifar10
+MODEL: Resnet50Cifar100
 training_context: TrainingContextPrunedTrain
 dataset_context: DatasetSmallContext
 stages_context: StagesContextPrunedTrain
@@ -102,10 +102,10 @@ epoch_global: int = 0
 BATCH_PRINT_RATE = 100
 
 sparsity_configs = {
-    "pruning_end": 3,
-    "regrowing_end": 3,
-    "target_sparsity": 0.5,
-    "lr_flow_params_decay_regrowing": 0.95
+    "pruning_end": 400,
+    "regrowing_end": 600,
+    "target_sparsity": 1.0,
+    "lr_flow_params_decay_regrowing": 0.9
 }
 
 def train_resnet50_cifar100_sparse_model():
